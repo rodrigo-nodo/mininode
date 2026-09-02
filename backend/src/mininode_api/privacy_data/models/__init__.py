9062d02122047c4b3b445ecce2e144d336fca474
Rodrigo Tech <rodrigo.node@gmail.com>
2026-09-02 17:01:43 -0400

    feat: add Privacy Data recovery links

--- backend/src/mininode_api/privacy_data/models/__init__.py
+++ backend/src/mininode_api/privacy_data/models/__init__.py
@@ -1,9 +1,9 @@
 """Privacy Data API models."""
 
 from .activity import ActivityCreate, ActivityResponse, ActivityUpdate
-from .data_map import CreatedDataMapResponse, DataMapResponse, DataMapUpdate
+from .data_map import CreatedDataMapResponse, DataMapResponse, DataMapUpdate, RecoveryLinkResponse
 
 __all__ = [
     "ActivityCreate", "ActivityResponse", "ActivityUpdate",
-    "CreatedDataMapResponse", "DataMapResponse", "DataMapUpdate",
+    "CreatedDataMapResponse", "DataMapResponse", "DataMapUpdate", "RecoveryLinkResponse",
 ]
